62d68684c576d5d451071cdcec56c77aae99cb82
Marcio Fernandes <farciomernandes@gmail.com>
2023-07-12 09:55:18 -0300

    first commit

---
+++ src/pages/Main/Burgers/index.tsx
@@ -0,0 +1,11 @@
+import { Head } from '../../../components/Head'
+import { SnackTitle } from '../../../components/SnackTitle'
+
+export default function Burgers() {
+  return (
+    <>
+      <Head title='Hambúrgueres' />
+      <SnackTitle>Hambúrgueres</SnackTitle>
+    </>
+  )
+}
