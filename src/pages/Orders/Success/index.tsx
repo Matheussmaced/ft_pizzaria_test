3f0ac263d2a527bb3f31fee4b29da0727a98bf21
Marcio Fernandes <farciomernandes@gmail.com>
2023-09-28 23:17:51 -0300

    fix: update info shop

--- src/pages/Orders/Success/index.tsx
+++ src/pages/Orders/Success/index.tsx
@@ -21,8 +21,8 @@ export default function OrderSuccessPage() {
         <SubTitle>Dados de Contato da Loja</SubTitle>
 
         <ul>
-          <li>Endereço: Av Central, 123</li>
-          <li>Tel: 11 98412-4578</li>
+          <li>Endereço: R Paulo José de Macêdo, Aurora velha, 17</li>
+          <li>Tel: 83 98161-3615</li>
         </ul>
 
         <br />
